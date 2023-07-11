ac21d42b5b49551d5ae79059a8443aa0ee079681
Sipang K <plaaom39@gmail.com>
2023-07-12 01:58:58 +0700

    chore: show the contents of all blogs

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,5 +1,20 @@
 import React from "react";
+import blogs from "../assets/data/blogs";
+import "../styles/Blogs.css";
 
 export default function Blogs() {
-  return <div>Blogs</div>;
+  return (
+    <div className="blogs-container">
+      <article>
+        {blogs.map((blog) => {
+          return (
+            <div className="card" key={blog.id}>
+              <h2>{blog.title}</h2>
+              <p>{blog.content.substring(0, 150)}</p>
+            </div>
+          );
+        })}
+      </article>
+    </div>
+  );
 }
