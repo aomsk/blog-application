48bd1a8f774dcd0b464cab113fa41b10b3ceb230
Sipang K <plaaom39@gmail.com>
2023-07-12 01:38:10 +0700

    chore: add Navigate to redirect path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Blogs from "./pages/Blogs";
 import Navbar from "./components/Navbar";
 import NotFound from "./components/NotFound";
+
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +15,8 @@ function App() {
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/home" element={<Navigate to="/" />} />
+        <Route path="/info" element={<Navigate to="/about" />} />
       </Routes>
     </BrowserRouter>
   );
